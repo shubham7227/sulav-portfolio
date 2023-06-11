4fac9f9796eddeb87918d9b099becbb4e78106f7
Sulav Giri <girishubham77@gmail.com>
2023-06-11 23:03:53 +0545

    favicon added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,41 +5,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-Y20ZWK8XJK"
-      />
 
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-Y20ZWK8XJK');
-        `}
-      </Script>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      {/* <link
+      <link
         rel="preload"
         as="script"
         href="https://www.googletagmanager.com/gtag/js?id=G-Y20ZWK8XJK"
-      /> */}
-      <title>Full-Stack Software Engineer - Sulav Giri</title>
-      <link rel="canonical" href="https://sulavgiri.com.np" />
-      <meta
-        name="description"
-        content="Sulav Giri is a software engineer from Nepal with expertise in web development using Node.js and React"
       />
-      <meta property="og:site_name" content="Sulav Giri" />
-      <meta property="og:locale" content="en-US" />
-      <meta property="og:image" content="/WebsiteBanner.png" />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="630" />
-      <meta property="og:type" content="website" />
-      <meta property="twitter:card" content="summary_large_image" />
-      <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="any" />
-
+      <link rel="canonical" href="https://sulavgiri.com.np" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link
         rel="preconnect"
@@ -52,8 +24,20 @@ export default function Document() {
       />
 
       <body>
-        {/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5TQWNXH"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-Y20ZWK8XJK"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-Y20ZWK8XJK');
+        `}
+        </Script>
         <Main />
         <NextScript />
       </body>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,19 +7,59 @@ import Header from "@/components/hero/Header";
 import Navbar from "@/components/hero/Navbar";
 import Projects from "@/components/projects";
 import Skills from "@/components/skills";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <main className="bg-background ">
-      <Header />
-      <Hero />
-      <Navbar />
-      <Experience />
-      <Skills />
-      <Projects />
-      <About />
-      <Contact />
-      <MailIcon />
-    </main>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Full-Stack Software Engineer - Sulav Giri</title>
+        <meta
+          name="description"
+          content="Sulav Giri is a software engineer from Nepal with expertise in web development using Node.js and React"
+        />
+        <meta property="og:site_name" content="Sulav Giri" />
+        <meta property="og:locale" content="en-US" />
+        <meta property="og:image" content="/WebsiteBanner.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:type" content="website" />
+        <meta property="twitter:card" content="summary_large_image" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#1f1f1f" />
+      </Head>
+      <main className="bg-background ">
+        <Header />
+        <Hero />
+        <Navbar />
+        <Experience />
+        <Skills />
+        <Projects />
+        <About />
+        <Contact />
+        <MailIcon />
+      </main>
+    </>
   );
 }
