517ca7021d1abffc2a7eeb8d8129fa6eff774005
Sulav Giri <girishubham77@gmail.com>
2023-06-12 16:54:25 +0545

    Vercel analytics added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,7 @@ import "slick-carousel/slick/slick-theme.css";
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
       <Analytics />
     </>
   );
