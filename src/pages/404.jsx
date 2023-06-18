2f8c4acbd1eb0ef356c682d52185dfe3739b6b9c
Sulav Giri <girishubham77@gmail.com>
2023-06-18 15:07:31 +0545

    Error fixed in 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,4 +1,4 @@
-import SimpleHeader from "@/components/common/SimpleHeader";
+import Header from "@/components/common/header";
 import Head from "next/head";
 import React from "react";
 
@@ -9,7 +9,7 @@ export default function NotFound() {
         <title>Page Not Found - Sulav Giri</title>
       </Head>
       <section className="h-[100vh] w-full flex justify-center items-center bg-background">
-        <SimpleHeader />
+        <Header />
         <div className="flex flex-row items-center leading-none">
           <h1 className="font-prompt font-semibold sm:text-[48px] text-[36px] text-primary pr-4 border-r-textDim border-r">
             404
