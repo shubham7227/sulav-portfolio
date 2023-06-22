f159ac22e4869cfbabc855397488717373888444
Sulav Giri <girishubham77@gmail.com>
2023-06-22 17:30:47 +0545

    modified the hero animation and content

--- src/components/hero/Info.jsx
+++ src/components/hero/Info.jsx
@@ -15,15 +15,15 @@ const Info = () => {
         <h1 className="font-prompt font-semibold lg:text-[4rem] text-[2.3rem] text-primary leading-tight">
           I write code that solves problem
         </h1>
-        <p className="lg:text-[20px] sm:text-[18px] text-textSecondary font-karla max-w-[90%]">
-          I am a computer engineering graduate with exceptional problem solving
-          skills. I primarily develop solutions using MERN stack.
+        <p className="sm:text-lg text-textSecondary font-karla max-w-[90%]">
+          I am a computer engineering graduate and I primarily develop solutions
+          using MERN stack.
         </p>
       </div>
       <div className="flex-none z-10">
         <Link
           href="#contact"
-          className="w-max flex flex-row gap-3 items-center text-primary rounded-xl py-2 pl-3 pr-4  bg-backgroundSecondary border border-backgroundLight duration-150 transition-all font-light font-karla text-sm hover:bg-backgroundLight z-10"
+          className="w-max flex flex-row gap-3 items-center text-primary rounded-xl py-2 pl-3 pr-4  bg-backgroundSecondary border border-backgroundLight duration-150 transition-all font-light font-karla hover:bg-backgroundLight z-10"
           scroll={false}
         >
           <p className="flex">Connect</p>
